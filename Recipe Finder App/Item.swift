fb270acef44a9f7eea358d8b2d214a2e0633b15e
Seugnkyu Park <seungkyu.park@growdle.com>
2025-01-06 11:54:02 +0900

    Initial Commit

---
+++ Recipe Finder App/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Recipe Finder App
+//
+//  Created by Park Seung Kyu on 1/6/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ Recipe Finder AppTests/Recipe_Finder_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe_Finder_AppTests.swift
+//  Recipe Finder AppTests
+//
+//  Created by Park Seung Kyu on 1/6/25.
+//
+
+import Testing
+@testable import Recipe_Finder_App
+
+struct Recipe_Finder_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
